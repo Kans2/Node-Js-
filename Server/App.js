49f0695ef9c227fc022d1c9df47e24718e08a9e2
Kans2 <142505200+Kans2@users.noreply.github.com>
2024-07-21 04:17:40 +0000

    updated

--- Server/App.js
+++ Server/App.js
@@ -1,12 +1,15 @@
-var http = require('http');  //module request
+const http = require('http');  //module request
 
-http.createServer(function(req,res){
-    res.statusCode= 200;
-   // res.setHeader = ('content-type', 'text/plain');
+const server = http.createServer((req,res)=>{
+
+   res.statusCode= 200;
+   res.setHeader = ('Content-Type', 'text/plain');
    res.write('Hello world! buddy')
    res.end();
 
-}).listen(8080,()=>{
+})
+
+server.listen(8080,()=>{
     console.log('server listen at 8080');
 });
 
